68daa4b87ae4b6fa68ed76958d3e70c1fe44e4ed
Keith Rooney <keithrooney@protonmail.com>
2025-11-11 00:08:31 +0000

    introducing diff backups every monday to saturday

--- ansible/roles/pgbackrest/files/etc/pgbackrest/scripts/backup.bash
+++ ansible/roles/pgbackrest/files/etc/pgbackrest/scripts/backup.bash
@@ -1,8 +1,37 @@
 #!/bin/bash
+ME=$(basename "$0")
+
 function log {
-    msg=$1
+    level=$1
+    msg=$2
     echo "`date +'%Y-%m-%d %H:%M:%S.%3N'` $$    INFO: $msg"
 }
+
+FULL_BACKUP_KEYWORD="full"
+DIFF_BACKUP_KEYWORD="diff"
+DEFAULT_BACKUP_TYPE=$FULL_BACKUP_KEYWORD
+type=$DEFAULT_BACKUP_TYPE
+while getopts ":t:h" opt;
+do
+    case $opt in
+    t)
+        type="$OPTARG"
+        if [[ $type != $DIFF_BACKUP_KEYWORD ]] && [[ $type != $FULL_BACKUP_KEYWORD ]];
+        then
+            echo "error: invalid argument: try '$ME -h' for more information"
+            exit 1 
+        fi
+        ;;
+    h)
+        echo "usage: $ME [-t full|diff]"
+        exit 0
+        ;;
+    *)
+        echo "error: invalid option: try '$ME -h' for more information"
+        exit 1
+        ;;
+    esac
+done
 
 leader_host=$(/opt/venv/patroni/bin/patronictl -c /etc/patroni/patroni.yml list -f json | jq -r '.[] | select(.Role == "Leader") | .Host')
 if [[ "$leader_host" != "$HOSTNAME" ]];
@@ -14,4 +43,4 @@ if [[ -z "$(ls -A /var/lib/pgbackrest)" ]];
 then
     pgbackrest stanza-create --stanza=main --log-level-console=info
 fi
-pgbackrest backup --type=full --stanza=main --log-level-console=info
+pgbackrest backup --type=$type --stanza=main --log-level-console=info
