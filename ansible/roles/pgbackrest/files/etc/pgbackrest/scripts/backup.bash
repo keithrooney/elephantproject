2dd3a347ad1903c695714e975675a9b8d8d01461
Keith Rooney <keithrooney@protonmail.com>
2026-01-29 22:57:55 +0000

    updated the backup script to include checks for streaming replication

--- ansible/roles/pgbackrest/files/etc/pgbackrest/scripts/backup.bash
+++ ansible/roles/pgbackrest/files/etc/pgbackrest/scripts/backup.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+set -e 
+
 ME=$(basename "$0")
 
 function log {
-    level=$1
-    msg=$2
+    msg=$1
     echo "`date +'%Y-%m-%d %H:%M:%S.%3N'` $$    INFO: $msg"
 }
 
@@ -33,14 +35,31 @@ do
     esac
 done
 
-leader_host=$(/opt/venv/patroni/bin/patronictl -c /etc/patroni/patroni.yml list -f json | jq -r '.[] | select(.Role == "Leader") | .Host')
-if [[ "$leader_host" != "$HOSTNAME" ]];
+DEFAULT_REPLICA_GOODBYE_MESSAGE="I'm a replica, I can't do anything, goodbye!"
+DEFAULT_STANDBY_SIGNAL_PATH="$PGDATA/standby.signal"
+DEFAULT_PATRONICTL_PATH="/opt/venv/patroni/bin/patronictl"
+
+# If neither of none of these conditions hold true, we assume the node is the leader and proceed with backup
+if [[ -f "$DEFAULT_PATRONICTL_PATH" ]];
 then
-    log "I'm a replica, I can't do anything, goodbye!"
+    # If Patroni is being used, verify the node is not the leader through the API.
+    leader_host=$(/opt/venv/patroni/bin/patronictl -c /etc/patroni/patroni.yml list -f json | jq -r '.[] | select(.Role == "Leader") | .Host')
+    if [[ "$leader_host" != "$HOSTNAME" ]];
+    then
+        log "$DEFAULT_REPLICA_GOODBYE_MESSAGE"
+        exit 0
+    fi
+elif [[ -f "$DEFAULT_STANDBY_SIGNAL_PATH" ]];
+then
+    # If streaming replication is being used, verify the node is not the leader by checking if the "standby.signal" file exists.
+    log "$DEFAULT_REPLICA_GOODBYE_MESSAGE"
     exit 0
 fi
+
 if [[ -z "$(ls -A /var/lib/pgbackrest)" ]];
 then
+    # Create the initial stanza before the backup if it doesn't exist
     pgbackrest stanza-create --stanza=main --log-level-console=info
 fi
+
 pgbackrest backup --type=$type --stanza=main --log-level-console=info
